65e25da208cc932777969124f00e41834a787a29
Diego <diegoaragao465@gmail.com>
2022-10-22 05:14:12 -0300

    Implement test for RoleService

--- src/test/java/me/diego/pokedex/service/RoleServiceTest.java
+++ src/test/java/me/diego/pokedex/service/RoleServiceTest.java
@@ -1,13 +1,23 @@
 package me.diego.pokedex.service;
 
+import me.diego.pokedex.enums.RoleName;
+import me.diego.pokedex.exception.BadRequestException;
+import me.diego.pokedex.model.Role;
 import me.diego.pokedex.repository.RoleRepository;
+import me.diego.pokedex.util.RoleCreator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 class RoleServiceTest {
@@ -20,6 +30,22 @@ class RoleServiceTest {
 
     @BeforeEach
     void setUp() {
-        
+        BDDMockito.when(roleRepository.findByRoleName(ArgumentMatchers.any(RoleName.class)))
+                .thenReturn(Optional.of(RoleCreator.createValidUserRole()));
+    }
+
+    @Test
+    @DisplayName("findRoleByName returns role when successful")
+    void findRoleByName_ReturnsRole_WhenSuccessful() {
+        Role roleFound = roleService.findRoleByName(RoleName.ROLE_USER);
+
+        Role expectedRole = RoleCreator.createValidUserRole();
+
+        Assertions.assertThat(roleFound)
+                .isNotNull();
+
+        Assertions.assertThat(roleFound.getRoleName())
+                .isEqualTo(expectedRole.getRoleName());
+
     }
 }
